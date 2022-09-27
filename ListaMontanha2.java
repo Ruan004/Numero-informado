a6166b7f4a603dce52792bd9a1fc60fb6a916322
Ruan004 <104209771+Ruan004@users.noreply.github.com>
2022-09-27 20:13:32 -0300

    Add files via upload

---
+++ ListaMontanha2.java
@@ -0,0 +1,18 @@
+
+package listamontanha2;
+
+import java.util.Scanner;
+
+
+public class ListaMontanha2 {
+
+    
+    public static void main(String[] args) {
+        Scanner leia = new Scanner (System.in);
+        int numero;
+        System.out.print("Digite um numero: ");
+        numero = leia.nextInt();
+        System.out.println("O numero informado foi: " + numero);
+    }
+    
+}
